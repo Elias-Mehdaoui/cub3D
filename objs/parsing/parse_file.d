4dc9c0d0cb73455d915e6d9e0d6ba3bd649ab3de
Elias Mehdaoui <emehdaou@made-f0Cr10s1.clusters.42paris.fr>
2024-12-05 18:21:29 +0100

    push fclean

--- objs/parsing/parse_file.d
+++
@@ -1,6 +0,0 @@
-objs/parsing/parse_file.o: srcs/parsing/parse_file.c \
-  srcs/parsing/../../includes/cub3d.h includes/../libft/libft.h
-
-srcs/parsing/../../includes/cub3d.h:
-
-includes/../libft/libft.h:
